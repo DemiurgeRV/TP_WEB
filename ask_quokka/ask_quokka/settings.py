7c0150afa144c1e3ed4e464a372ee6d02bb94bb4
Rybin Vladislav <rybinvladislav@mail.ru>
2025-04-09 19:08:46 +0300

    first template

--- ask_quokka/ask_quokka/settings.py
+++ ask_quokka/ask_quokka/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q1)574xx%dut-t2jn$mejgnyqe*_ixgf@)_w)-p7-2yu$ypb28'
+SECRET_KEY = 'django-insecure-wi-gqf69!q%#kg(+x33#j52h4&doa9p$a+%*z60a$!b-#1kz7='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'ask_quokka.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
