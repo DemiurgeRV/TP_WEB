e981ed470a874e16038b8667e3a5b4117acf4fa5
Rybin Vladislav <rybinvladislav@mail.ru>
2025-04-09 20:22:45 +0300

    add fake db

--- ask_quokka/app/views.py
+++ ask_quokka/app/views.py
@@ -1,6 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+questions = []
+for i in range(1,30):
+    questions.append({
+        'title': 'Как работает шаблонизация Django? ' + str(i),
+        'id': i,
+        'text':  str(i) + ''' Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...'''
+    })
+
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index.html', context={'questions': questions})
