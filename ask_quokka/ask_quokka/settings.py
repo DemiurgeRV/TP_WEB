cc7a43e8a4977aa6b2688934df436703a02d5e87
Rybin Vladislav <rybinvladislav@mail.ru>
2025-04-09 17:27:53 +0300

    create app and some dir

--- ask_quokka/ask_quokka/settings.py
+++ ask_quokka/ask_quokka/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*in&#a)xck@ax&j4ag@9u&^y%vamab-^^449ys1#kb2^ge-7e7'
+SECRET_KEY = 'django-insecure-q1)574xx%dut-t2jn$mejgnyqe*_ixgf@)_w)-p7-2yu$ypb28'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
