afdbf7e1a1d3b722c96cb6fdaef2a8ad23513ebe
Rybin Vladislav <rybinvladislav@mail.ru>
2025-06-16 14:23:55 +0300

    profile settings template

--- ask_quokka/ask_quokka/urls.py
+++ ask_quokka/ask_quokka/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('question/<int:question_id>', views.question, name='question'),
     path('ask', views.ask, name='ask'),
     path('signup', views.signup, name='signup'),
-    path('login', views.login, name='login')
+    path('login', views.login, name='login'),
 ]
