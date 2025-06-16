510ad1e3ed4ebb02e43aeb149bc615faae677774
Rybin Vladislav <rybinvladislav@mail.ru>
2025-06-16 14:55:58 +0300

    pagination

--- ask_quokka/app/views.py
+++ ask_quokka/app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .utils import paginate
 
 questions = []
 for i in range(1,30):
@@ -19,17 +20,30 @@ for i in range(1,16):
     })
 
 # Create your views here.
+# def index(request):
+#     return render(request, 'index.html', context={'questions': questions,
+#                                                   'tags': tags})
+#
+# def hot(request):
+#     return render(request, 'hot.html', context={'questions': questions,
+#                                                 'tags': tags})
+#
+# def tag(request, tag_name):
+#     return render(request, 'tag.html', context={'questions': questions, 'tags': tags,
+#                                                 'item': tag_name})
+
 def index(request):
-    return render(request, 'index.html', context={'questions': questions,
-                                                  'tags': tags})
+    page = paginate(questions, request)
+    return render(request, 'index.html', {'page': page, 'tags': tags})
 
 def hot(request):
-    return render(request, 'hot.html', context={'questions': questions,
-                                                'tags': tags})
+    page = paginate(questions, request)
+    return render(request, 'hot.html', {'page': page, 'tags': tags})
 
 def tag(request, tag_name):
-    return render(request, 'tag.html', context={'questions': questions, 'tags': tags,
-                                                'item': tag_name})
+    page = paginate(questions, request)
+    return render(request, 'tag.html', {'page': page, 'tags': tags, 'item': tag_name})
+
 def question(request, question_id):
     return render(request, 'question.html', context={'question': questions[question_id], 'tags': tags})
 
