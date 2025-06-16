af987e2ba380fc37be0de29463647aef1178a649
Rybin Vladislav <rybinvladislav@mail.ru>
2025-06-16 23:56:51 +0300

    fix tag

--- ask_quokka/app/urls.py
+++ ask_quokka/app/urls.py
@@ -3,11 +3,12 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('hot', views.hot, name='hot'),
-    path('tag/<str:tag_name>', views.tag, name='tag'),
-    path('question/<int:question_id>', views.question, name='question'),
-    path('ask', views.ask, name='ask'),
+    path('hot/', views.hot, name='hot'),
+    path('tag/<str:tag_name>/', views.tag, name='tag'),
+    path('question/<int:question_id>/', views.question, name='question'),
+    path('ask/', views.ask, name='ask'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
     path('settings/', views.settings, name='settings'),
 ]
+
--- ask_quokka/app/views.py
+++ ask_quokka/app/views.py
@@ -1,23 +1,24 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .utils import paginate
+from .models import Question, Tag, Answer
 
-questions = []
-for i in range(1,30):
-    questions.append({
-        'title': 'Как работает шаблонизация Django? ' + str(i),
-        'id': i,
-        'text':  str(i) + ''' Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                              sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...'''
-    })
-
-tags = []
-for i in range(1,16):
-    tags.append({
-        'name': 'Tag ' + str(i),
-        'id': i,
-    })
+# questions = []
+# for i in range(1,30):
+#     questions.append({
+#         'title': 'Как работает шаблонизация Django? ' + str(i),
+#         'id': i,
+#         'text':  str(i) + ''' Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+#                               sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+#                               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris...'''
+#     })
+#
+# tags = []
+# for i in range(1,16):
+#     tags.append({
+#         'name': 'Tag ' + str(i),
+#         'id': i,
+#     })
 
 # Create your views here.
 # def index(request):
@@ -32,20 +33,52 @@ for i in range(1,16):
 #     return render(request, 'tag.html', context={'questions': questions, 'tags': tags,
 #                                                 'item': tag_name})
 
+# def index(request):
+#     page = paginate(questions, request)
+#     return render(request, 'index.html', {'page': page, 'tags': tags})
+#
+# def hot(request):
+#     page = paginate(questions, request)
+#     return render(request, 'hot.html', {'page': page, 'tags': tags})
+
+# def tag(request, tag_name):
+#     page = paginate(questions, request)
+#     return render(request, 'tag.html', {'page': page, 'tags': tags, 'item': tag_name})
+# def question(request, question_id):
+#     return render(request, 'question.html', context={'question': questions[question_id], 'tags': tags})
+
 def index(request):
+    questions = Question.objects.new()
     page = paginate(questions, request)
+    tags = Tag.objects.all()
     return render(request, 'index.html', {'page': page, 'tags': tags})
 
 def hot(request):
+    questions = Question.objects.hot()
     page = paginate(questions, request)
+    tags = Tag.objects.all()
     return render(request, 'hot.html', {'page': page, 'tags': tags})
 
 def tag(request, tag_name):
+    tag = get_object_or_404(Tag, name=tag_name)
+    questions = tag.questions.all()
     page = paginate(questions, request)
-    return render(request, 'tag.html', {'page': page, 'tags': tags, 'item': tag_name})
+    all_tags = Tag.objects.all()
+    return render(request, 'tag.html', {
+        'page': page,
+        'tag': tag,
+        'item': tag.name,
+        'tags': all_tags,  # <-- добавлено
+    })
 
 def question(request, question_id):
-    return render(request, 'question.html', context={'question': questions[question_id], 'tags': tags})
+    question = get_object_or_404(Question, id=question_id)
+    answers = Answer.objects.filter(question=question).order_by('-created_at')  # или другой порядок
+    page = paginate(answers, request)
+    return render(request, 'question.html', {
+        'question': question,
+        'page': page
+    })
 
 def ask(request):
     return render(request, 'ask.html', context={'questions': questions,
